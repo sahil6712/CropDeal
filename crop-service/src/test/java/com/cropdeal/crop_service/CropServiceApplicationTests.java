46a8f2979990a45d0f73d5e70b76225129f6e909
Sahil <sahilkhemariya327@gmail.com>
2025-03-11 13:49:40 +0530

    [Sahil] Add. Spring boot microservices added

---
+++ crop-service/src/test/java/com/cropdeal/crop_service/CropServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropdeal.crop_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/cropdeal/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropdeal.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
